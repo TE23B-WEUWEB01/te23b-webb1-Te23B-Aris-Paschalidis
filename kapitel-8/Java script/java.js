8d25cf0bf4e2b10c786e8378adeb29de064039ca
Aris Paschalidis <aris.paschalidis@elev.ga.ntig.se>
2025-03-13 11:07:27 +0100

    tränat js, variabler och konktenering (sätta ihop text

---
+++ kapitel-8/Java script/java.js
@@ -0,0 +1,14 @@
+// var ålder = 20
+// var tal1 = 12
+// var tal2 = 24
+// var summa = tal1 + tal2;
+// alert(tal1 + "+" + tal2 + "=" + summa);
+
+var förnamn = "Karim"
+var efternamn = "Ryde"
+var helaNamnet = förnamn + " " + efternamn;
+var hobby = "thai mat";
+var kostnad = 4000;
+
+var mening =; helaNamnet + "'hobby är " + hobby + ".nMin hobby kostar "
+alert(mening);
